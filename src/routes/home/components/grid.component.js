a004d5e4f85986d36dd44622d115ae64fb250e73
Florent Barriol <fbarriol.externe@m6.fr>
2019-05-16 14:02:02 +0200

    wip

--- src/routes/home/components/grid.component.js
+++ src/routes/home/components/grid.component.js
@@ -6,8 +6,8 @@ import { IMAGES_SEARCH_URL, fetchAPI } from '../../../api';
 import { ResultBox } from './resultBox.component';
 
 const APIResource = unstable_createResource(
-  filters => fetchAPI(`${IMAGES_SEARCH_URL}?${queryString.stringify(filters)}`),
-  input => (input.breed_id ? input.breed_id : -1)
+  filters => fetchAPI(`${IMAGES_SEARCH_URL}?${queryString.stringify(filters)}`)
+  // input => (input.breed_id ? input.breed_id : -1)
 );
 
 export const Grid = ({ filters }) => {
